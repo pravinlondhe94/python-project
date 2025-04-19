f5d7f50785009a642c8b5781628084b2d7975ef4
jay415404 <jay416505@gmail.com>
2025-04-19 09:25:09 +0530

    feature: trigger lambda by apigateway

--- userdata/app.py
+++ userdata/app.py
@@ -1,5 +1,5 @@
 import boto3
-import os
+import os, json
 
 dynamodb = boto3.resource('dynamodb')
 table_name = os.environ.get('USERS_TABLE', 'users')
@@ -7,16 +7,26 @@ table = dynamodb.Table(table_name)
 
 
 def lambda_handler(event, context):
+
+    body = event.get("body")
+    print(f"Request body: {body}")
+    # If it's a JSON string, parse it:
+    if body:
+        data = json.loads(body)
+    else:
+        data = event
+    print(f"data : {data}")
+
     required_fields = ['first_name', 'last_name', 'email']
 
     for field in required_fields:
-        if field not in event:
+        if field not in data:
             raise ValueError(f"Missing required field: {field}")
 
     item = {
-        'email': event['email'],
-        'first_name': event['first_name'],
-        'last_name': event['last_name']
+        'email': data['email'],
+        'first_name': data['first_name'],
+        'last_name': data['last_name']
     }
 
     table.put_item(Item=item)
