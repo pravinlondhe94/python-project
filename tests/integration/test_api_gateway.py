7bd1334265a5348a65851eab71fd43375c71d44c
jay415404 <jay416505@gmail.com>
2025-04-19 11:54:58 +0530

    chor: updated readme.md

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -2,7 +2,6 @@ import os, json
 from unittest import TestCase
 import boto3
 import requests
-from pathlib import Path
 
 """
 Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we are going to test. 
